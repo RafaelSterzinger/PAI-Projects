04662afc65596d105142cc4bf1b25f8338c816f7
Fatjon ZOGAJ <fatjon.zogaj@hotmail.com>
2020-10-07 10:07:20 +0200

    Add task description

--- solution.py
+++ solution.py
@@ -15,30 +15,31 @@ def cost_function(true, predicted):
 
         return: float
     """
-    cost = (true - predicted)**2
+    cost = (true - predicted) ** 2
 
     # true above threshold (case 1)
     mask = true > THRESHOLD
-    mask_w1 = np.logical_and(predicted>=true,mask)
-    mask_w2 = np.logical_and(np.logical_and(predicted<true,predicted >=THRESHOLD),mask)
-    mask_w3 = np.logical_and(predicted<THRESHOLD,mask)
+    mask_w1 = np.logical_and(predicted >= true, mask)
+    mask_w2 = np.logical_and(np.logical_and(predicted < true, predicted >= THRESHOLD), mask)
+    mask_w3 = np.logical_and(predicted < THRESHOLD, mask)
 
-    cost[mask_w1] = cost[mask_w1]*W1
-    cost[mask_w2] = cost[mask_w2]*W2
-    cost[mask_w3] = cost[mask_w3]*W3
+    cost[mask_w1] = cost[mask_w1] * W1
+    cost[mask_w2] = cost[mask_w2] * W2
+    cost[mask_w3] = cost[mask_w3] * W3
 
     # true value below threshold (case 2)
     mask = true <= THRESHOLD
-    mask_w1 = np.logical_and(predicted>true,mask)
-    mask_w2 = np.logical_and(predicted<=true,mask)
+    mask_w1 = np.logical_and(predicted > true, mask)
+    mask_w2 = np.logical_and(predicted <= true, mask)
 
-    cost[mask_w1] = cost[mask_w1]*W1
-    cost[mask_w2] = cost[mask_w2]*W2
+    cost[mask_w1] = cost[mask_w1] * W1
+    cost[mask_w2] = cost[mask_w2] * W2
 
-    reward = W4*np.logical_and(predicted < THRESHOLD,true<THRESHOLD)
+    reward = W4 * np.logical_and(predicted < THRESHOLD, true < THRESHOLD)
     if reward is None:
         reward = 0
     return np.mean(cost) - np.mean(reward)
+
 
 """
 Fill in the methods of the Model. Please do not change the given methods for the checker script to work.
@@ -54,6 +55,24 @@ It uses predictions to compare to the ground truth using the cost_function above
 
 
 class Model():
+    """
+    We envisage that in order to solve this task you need to overcome three challenges - each requiring a specific strategy.
+
+    1. Model selection - You will need to find the right kernel and its hyper-parameters that model the GP
+    faithfully. With Bayesian models, a commonly used principle in choosing the right kernel or hyper-parameters is
+    to use the "data likelihood", otherwise known as the marginal likelihood to find the best model. See more details
+    at: Wiki
+    2. Large scale learning - Natively, GP inference is computationally intensive for large datasets and
+    common-place computers. The inference requires O(N3) basic operations in order find the posterior distributions.
+    For large datasets this becomes infeasible. In order to solve this problem, practitioners use forms of low-rank
+    approximations. The most popular are the Nyström method, using random features and/or other scalable
+    clustering-based approaches. This excellent review on Wikipedia can serve as an introduction: Wiki.
+    3. Asymmetric
+    costs: We utilize a specifically designed cost function, where deviation from the true concentration levels is
+    penalized, and you are rewarded for correctly predicting safe regions. Under this specific cost function,
+    the mean prediction might not be optimal. Note that the mean prediction refers to the optimal decision with
+    respect to a general squared loss and some posterior distribution over the true value to be predicted.
+    """
 
     def __init__(self):
         """
