9f87ad2cf5695dbdd63af5afc0fb62bd949ce023
rafael <rafael.sterzinger@gmx.at>
2020-10-07 16:50:01 +0200

    Add Nyström kernel approximation

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.gaussian_process as gp
+from sklearn.kernel_approximation import Nystroem
 
 ## Constant for Cost function
 THRESHOLD = 0.5
@@ -83,26 +84,29 @@ class Model():
         self.__kernel = gp.kernels.ConstantKernel(1.0, (1e-1, 1e3)) * gp.kernels.RBF(10.0, (1e-3, 1e3))
         # alpha is the variance of the i.i.d. noise on the labels, and normalize_y refers to the constant mean function
         # — either zero if False or the training data mean if True.
-        self.__model = gp.GaussianProcessRegressor(kernel=self.__kernel, n_restarts_optimizer=10, alpha=0.1, normalize_y=True)
-        #pass
+        self.__kernel_approximation = Nystroem(gamma=.2, random_state=1, n_components=300)
+        self.__model = gp.GaussianProcessRegressor(n_restarts_optimizer=0,
+                                                   alpha=0.1, normalize_y=False)
+        # pass
 
     def predict(self, test_x):
         """
             TODO: enter your code here
         """
         ## dummy code below
-        #y = np.ones(test_x.shape[0]) * THRESHOLD - 0.00001
-        y_pred, std = self.__model.predict(test_x, return_std=True)
+        # y = np.ones(test_x.shape[0]) * THRESHOLD - 0.00001
+        y_pred, std = self.__model.predict(self.__kernel_approximation.transform(test_x), return_std=True)
         return y_pred
 
     def fit_model(self, train_x, train_y):
         """
              TODO: enter your code here
         """
-        self.__model.fit(train_x, train_y)
+        data = self.__kernel_approximation.fit_transform(train_x, train_y)
+        self.__model.fit(data, train_y)
         params = self.__model.kernel_.get_params()
 
-        #pass
+        # pass
 
 
 def visualize(train_x, train_y):
@@ -111,6 +115,7 @@ def visualize(train_x, train_y):
     label = list(map(lambda x: ('g' if x <= 0.5 else 'r'), train_y))
     plt.scatter(train_x1, train_x2, s=0.1, c=label)
     plt.show()
+
 
 def main():
     train_x_name = "train_x.csv"
